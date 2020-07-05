bca5cd5b978fb7acff4ebb284c048d2f02480f8d
Shlok Mehrotra <shlokmehrotra@gmail.com>
2020-07-05 12:32:39 -0700

    initial code

---
+++ plot.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import csv
+arr = []
+with open("data.csv", newline='' ) as csvfile:
+	#first = csvfile.readlines()[0].split(',')
+	#print(first)
+	for row in csvfile.readlines()[1:5]:
+		row = row.rstrip()
+		arr.append(row.split(','))
+		print(row)
+	arr = np.array(arr)
+	print(arr)
+	dataframe = pd.DataFrame(arr, columns = "[gene_id,spot_id,time,spot_fluorescence]")
+print(dataframe)
